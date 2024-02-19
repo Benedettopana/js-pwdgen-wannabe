5327a704274c3855a19016a421dc838eb881cf92
Benedettopana <benedetto.pana@gmail.com>
2024-02-19 13:26:32 +0100

    [add]Milestone 1

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const myName = prompt('Scrivi qui il tuo nome:');
+
+const myLastname = prompt('Scrivi qui il tuo cognome:');
+
+const myFavColor = prompt('Scrivi qui il tuo colore preferito:');
+
+const passwordGen = myName + myLastname + myFavColor + '24';
+
+console.log(myName, myLastname, myFavColor);
+console.log(passwordGen);
